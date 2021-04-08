fb8b7f5b6deb45fe31072ea8590e52a00c9b89f4
Ryan Richetto <rrichetto15@gmail.com>
2021-04-07 23:03:47 -0700

    initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
+import { FaAlignJustify } from "react-icons/fa";
+import logo from "../assets/images/logo.svg";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/tags">Tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">Recipes</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="Simply Recipes" />
+          </Link>
+          <button className="nav-btn" onClick={() => setShow(!show)}>
+            <FaAlignJustify />
+          </button>
+        </div>
+        <div className={show ? "nav-links show-links" : "nav-links"}>
+          <Link
+            to="/"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
+            Home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
+            Recipes
+          </Link>
+          <Link
+            to="/tags"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
+            Tags
+          </Link>
+          <Link
+            to="/about"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
+            About
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn" onClick={() => setShow(false)}>
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,44 @@ import Layout from "../components/Layout";
 const Contact = () => {
   return (
     <Layout>
-      <h1>Contact Page</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to Get in Touch?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates, sint ducimus consequuntur laborum tempore labore!
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab,
+              cupiditate.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              cum dolores totam iste enim animi.
+            </p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">Your Name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">Your Email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button className="btn block" type="submit">
+                Submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   );
 };
